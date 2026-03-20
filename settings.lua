90bce4a1374786e73fac6bcb08f23794625a058e
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2026-03-20 13:03:26 -0500

    Version 0.1.1

--- settings.lua
+++ settings.lua
@@ -1,1 +1,16 @@
--- startup setting removed; Fulgora is always enabled
+data:extend({
+    {
+        type = "bool-setting",
+        name = "eon-fd-gleba-enemies-react-to-pollution",
+        setting_type = "startup",
+        default_value = false,
+        order = "a",
+    },
+    {
+        type = "bool-setting",
+        name = "eon-fd-use-tungsten-plate",
+        setting_type = "startup",
+        default_value = false,
+        order = "b",
+    },
+})
