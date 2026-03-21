9335db94dfdca2b0ae084f626033986d0748c259
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2026-03-21 17:00:57 -0500

    version 0.1.5

--- prototypes/technologies.lua
+++ prototypes/technologies.lua
@@ -34,20 +34,21 @@ data:extend({
                 recipe = "ice-platform",
             }
         },
-        prerequisites = { "cryogenic-science-pack", "planet-discovery-fulgora" },
+        prerequisites = { "advanced-asteroid-processing", "planet-discovery-fulgora" },
         unit =
         {
             count = 2000,
             ingredients =
             {
-                { "automation-science-pack",   1 },
-                { "logistic-science-pack",     1 },
-                { "chemical-science-pack",     1 },
-                { "production-science-pack",   1 },
-                { "utility-science-pack",      1 },
-                { "space-science-pack",        1 },
-                { "metallurgic-science-pack",  1 },
-                { "agricultural-science-pack", 1 }
+                { "automation-science-pack",      1 },
+                { "logistic-science-pack",        1 },
+                { "chemical-science-pack",        1 },
+                { "production-science-pack",      1 },
+                { "utility-science-pack",         1 },
+                { "space-science-pack",           1 },
+                { "metallurgic-science-pack",     1 },
+                { "agricultural-science-pack",    1 },
+                { "electromagnetic-science-pack", 1 }
             },
             time = 60
         }
